c8aa67a50bddb8d1296481841358c1193a26ff64
gabriel <gabriel.f.espanhol@hotmail.com>
2023-12-21 00:07:52 -0300

    trocando valores de testes

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -25,15 +25,15 @@ public class Menu {
 		float saldo, limite, valor;
 		
 		// Teste da Classe Conta Corrente
-		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "João Alves", 0.0f, 1000.0f);
+		ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "João da Silva", 0.0f, 1000.0f);
 		cc1.visualizar();
-		cc1.sacar(15000.0f);
+		cc1.sacar(12000.0f);
 		cc1.visualizar();
-		cc1.depositar(5500.0f);
+		cc1.depositar(5000.0f);
 		cc1.visualizar();
 		
         // Teste da Classe Conta Poupança
-		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria da Silva", 100000.0f, 15);
+		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
 		cp1.visualizar();
         cp1.sacar(1000.0f);
 		cp1.visualizar();
